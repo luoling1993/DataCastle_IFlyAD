bbdd88ed01750439a80f370eabf2385ddad1e2b2
Lucas Lu <lucas.lu@ribitwo.com>
2018-09-18 17:57:38 +0800

    update

--- features.py
+++ features.py
@@ -18,11 +18,6 @@ DONNET_COLUMN = ["instance_id", "time", "user_tags", "creative_width", "creative
 
 class TagsProcessing(object):
     # user_tags字段特殊处理
-    def __init__(self, user_tags):
-        if not isinstance(user_tags, pd.Series):
-            raise ValueError("user_tags must be pd.Series!")
-        self.user_tags = user_tags
-
     @staticmethod
     def _sorted_tags(tags):
         try:
@@ -31,10 +26,14 @@ class TagsProcessing(object):
         except AttributeError:
             return str(tags)
 
-    def get_topn_tags(self, topn=10):
+    @staticmethod
+    def get_topn_tags(user_tags, topn=100):
         # 获取频率最高的user_tags
+        if not isinstance(user_tags, pd.Series):
+            raise ValueError("user_tags must be pd.Series!")
+
         tags_list = list()
-        for _, tags in self.user_tags.iteritems():
+        for _, tags in user_tags.iteritems():
             try:
                 _tags_list = tags.split(",")
                 tags_list.extend(_tags_list)
@@ -45,18 +44,24 @@ class TagsProcessing(object):
         topn_tags = [item[0] for item in topn_counter]
         return topn_tags
 
-    def get_topn_tags_group(self, topn=10):
+    def get_topn_tags_group(self, user_tags, topn=100):
         # 获取频率最高的user_tags组合
+        if not isinstance(user_tags, pd.Series):
+            raise ValueError("user_tags must be pd.Series!")
+
         tags_group_list = list()
-        for _, tags in self.user_tags.iteritems():
+        for _, tags in user_tags.iteritems():
             sorted_tags = self._sorted_tags(tags)
+            if isinstance(sorted_tags, list):
+                sorted_tags = ",".join(map(str, sorted_tags))
             tags_group_list.append(sorted_tags)
         tags_group_counter = Counter(tags_group_list)
         topn_counter = tags_group_counter.most_common(topn)
         topn_tags_group = [item[0] for item in topn_counter]
         return topn_tags_group
 
-    def get_topn_tags_df(self, topn_tags):
+    @staticmethod
+    def get_topn_tags_df(user_tags, topn_tags):
         # 获取topn_tags构造的DataFrame
         columns = list()
         tags_df_list = list()
@@ -65,7 +70,7 @@ class TagsProcessing(object):
             coumn = "user_tags_{topn_tag}".format(topn_tag=topn_tag)
             columns.append(coumn)
 
-        for user_tag in self.user_tags:
+        for user_tag in user_tags:
             try:
                 user_tag_list = user_tag.split(",")
             except AttributeError:
@@ -83,8 +88,11 @@ class TagsProcessing(object):
         tags_df = pd.DataFrame(data=tags_df_list, columns=columns)
         return tags_df
 
-    def get_topn_tags_group_df(self, topn_tags_group):
+    def get_topn_tags_group_df(self, user_tags, topn_tags_group):
         # 获取topn_tags_group构造的DataFrame
+        if not isinstance(user_tags, pd.Series):
+            raise ValueError("user_tags must be pd.Series!")
+
         columns = list()
         tags_group_df_list = list()
 
@@ -92,8 +100,10 @@ class TagsProcessing(object):
             coumn = "topn_tags_group_{idx}".format(idx=idx)
             columns.append(coumn)
 
-        for user_tag in self.user_tags:
+        for user_tag in user_tags:
             sorted_user_tags = self._sorted_tags(user_tag)
+            if isinstance(sorted_user_tags, list):
+                sorted_user_tags = ",".join(map(str, sorted_user_tags))
 
             tags_group_list = list()
             for tags_group in topn_tags_group:
@@ -110,30 +120,38 @@ class TagsProcessing(object):
 
 class TimeProcessing(object):
     # time字段特殊处理
-    def __init__(self, times):
-        if not isinstance(times, pd.Series):
-            raise ValueError("times must be pd.Series!")
-        self.times = times
-
     @staticmethod
     def _get_init_timestamp():
         # 手动设定初始化时间戳
         # 表示x日0点0分0秒
-        init_timestamp = 0
+        init_timestamp = 2190038402
         return init_timestamp
 
-    def get_week_features(self, item):
+    def _get_week_features(self, item):
         # train为7天数据,test为1天数据
         init_timestamp = self._get_init_timestamp()
         interval_seconds = item - init_timestamp
         interval_days = int(interval_seconds / (3600 * 24))
-        week_num = interval_days % 6
+        week_num = interval_days % 7
         return week_num
 
-    def get_hour_features(self, item):
+    def _get_hour_features(self, item):
         # 通过时间戳换算小时数
         init_timestamp = self._get_init_timestamp()
-        
+        interval_seconds = item - init_timestamp
+        interval_hours_seconds = interval_seconds % (3600 * 24)
+        hour_num = int(interval_hours_seconds / 3600)
+        return hour_num
+
+    def get_times_df(self, times):
+        if not isinstance(times, pd.Series):
+            raise ValueError("times must be pd.Series!")
+
+        df = pd.DataFrame()
+        df["times_week"] = times.apply(self._get_week_features)
+        df["times_hours"] = times.apply(self._get_hour_features)
+        df = pd.get_dummies(df, columns=["times_week", "times_hours"])
+        return df
 
 
 class Processing(object):
@@ -173,7 +191,7 @@ class Processing(object):
 
     @staticmethod
     def _get_rate_dict(x, y, threshold=20):
-        avg_rate = np.average(y)
+        avg_rate = np.nanmean(y)
         x_set = set(x)
 
         rate_dict = dict()
@@ -181,7 +199,7 @@ class Processing(object):
             sub_y = y[x == item]
             if len(sub_y) <= threshold:
                 rate_dict[item] = avg_rate
-            sub_y_rate = np.average(sub_y)
+            sub_y_rate = np.nanmean(sub_y)
             rate_dict[item] = sub_y_rate
         return rate_dict
 
@@ -194,34 +212,54 @@ class Processing(object):
         train_name = os.path.join(etl_path, "train_features.csv")
         test_name = os.path.join(etl_path, "test_features.csv")
 
+        train_df_length = train_df.shape[0]
+        df = pd.concat([train_df, test_df], axis=0, ignore_index=True, sort=False)
+
+        df["creative_area"] = df["creative_width"] * df["creative_height"]
+        df["advert_industry_1"], df["advert_industry_2"] = df["advert_industry_inner"].str.split("_").str
+        df = df.drop(columns=["advert_industry_inner"])
+
         for column in RATE_COLUMN:
-            rate_dict = self._get_rate_dict(train_df[column], labels)
-            train_df[column] = train_df[column].map(rate_dict)
-            test_df[column] = test_df[column].map(rate_dict)
+            rate_dict = self._get_rate_dict(df[column], labels)
+            df[column] = df[column].map(rate_dict)
 
         for column in DUMPS_COLUMN:
-            train_column = train_df[column]
+            column_item = df[column]
 
-            if train_column.dtype == "bool":
-                train_df[column] = train_df[column].map({False: 0, True: 1})
-                test_df[column] = test_df[column].map({False: 0, True: 1})
+            if df[column].dtype == "bool":
+                df[column] = df[column].map({False: 0, True: 1})
                 continue
 
-            filter_num_list = self._get_filter_num(train_column)
-            train_df[column] = self._filter_num(train_df[column], filter_num_list)
-            test_df[column] = self._filter_num(test_df[column], filter_num_list)
+            filter_num_list = self._get_filter_num(column_item)
+            df[column] = self._filter_num(df[column], filter_num_list)
 
-        train_df = pd.get_dummies(train_df, columns=DUMPS_COLUMN)
-        test_df = pd.get_dummies(test_df, columns=DUMPS_COLUMN)
+        tags_processer = TagsProcessing()
+        topn_tags = tags_processer.get_topn_tags(df["user_tags"])
+        topn_tags_group = tags_processer.get_topn_tags_group(df["user_tags"])
+        topn_tags_df = tags_processer.get_topn_tags_df(df["user_tags"], topn_tags)
+        topn_tags_group_df = tags_processer.get_topn_tags_group_df(df["user_tags"], topn_tags_group)
+
+        df = pd.concat([df, topn_tags_df, topn_tags_group_df], axis=1)
+        df = df.drop(columns=["user_tags"])
+
+        time_processer = TimeProcessing()
+        times_df = time_processer.get_times_df(df["time"])
+        df = pd.concat([df, times_df], axis=1)
+        df = df.drop(columns=["time"])
+
+        DUMPS_COLUMN.append("advert_industry_1")
+        DUMPS_COLUMN.append("advert_industry_2")
+        df = pd.get_dummies(df, columns=DUMPS_COLUMN)
+
+        train_df = df[:train_df_length]
+        test_df = df[train_df_length:]
+
+        train_df["click"] = train_df["click"].apply(int)
+        test_df = test_df.drop(columns=["click"])
 
         train_df.to_csv(train_name, index=False)
         test_df.to_csv(test_name, index=False)
 
 
 if __name__ == "__main__":
-    pass
-    # test_df = Processing()._get_data(name="test")
-    # tag_processing = TagsProcessing(test_df["user_tags"])
-    # topn_tags = tag_processing.get_topn_tags(topn=10)
-    # df = tag_processing.get_topn_tags_df(topn_tags)
-    # print(df.head(10))
+    Processing().get_processing()
---
+++ xgb_models.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import log_loss
+
+
+def get_data(name):
+    etl_path = os.path.join("data", "EtlData")
+    if name == "train":
+        data_name = os.path.join(etl_path, "train_features.csv")
+    elif name == "test":
+        data_name = os.path.join(etl_path, "test_features.csv")
+    else:
+        raise ValueError("name must be `train` or `test`!")
+    df = pd.read_csv(data_name, header=0)
+    return df
+
+
+def fit_xgb_model(train_data, test_data, parms):
+    columns = train_data.columns
+    remove_fields = ["instance_id", "click"]
+    features_fields = [column for column in columns if column not in remove_fields]
+
+    train_features = train_data[features_fields]
+    train_labels = train_data["click"]
+    test_features = test_data[features_fields]
+
+    dtrain = xgb.DMatrix(train_features, label=train_labels)
+    dtest = xgb.DMatrix(test_features)
+    bst = xgb.train(parms, dtrain)
+
+    preds = bst.predict(dtest)
+    predictions = pd.DataFrame({"instance_id": test_data["instance_id"],
+                                "predicted_score": preds})
+    predictions.to_csv("predict.csv", index=False)
+
+
+def offline_tests(data_df, parms):
+    columns = data_df.columns
+    remove_fields = ["instance_id", "click"]
+    features_fields = [column for column in columns if column not in remove_fields]
+
+    train_data = data_df[data_df["times_week_6"] == 0]
+    test_data = data_df[data_df["times_week_6"] == 1]
+
+    train_features = train_data[features_fields]
+    train_labels = train_data["click"]
+    test_features = test_data[features_fields]
+    test_labels = test_data["click"]
+
+    dtrain = xgb.DMatrix(train_features, label=train_labels)
+    dtest = xgb.DMatrix(test_features, label=test_labels)
+    bst = xgb.train(parms, dtrain)
+
+    preds = bst.predict(dtest)
+    logloss = log_loss(test_labels, preds)
+
+    predictions = pd.DataFrame({"instance_id": test_data["instance_id"],
+                                "y_true": test_labels,
+                                "y_pred": preds})
+    predictions.to_csv("predict.csv", index=False)
+    return logloss
+
+
+if __name__ == "__main__":
+    xgb_parms = {
+        "booster": "gbtree",
+        "objective": "binary:logistic",
+        "eta": 0.01,
+        "n_estimators": 500,
+        "max_depth": 10,
+        "silent": 1,
+        "nthread": 10
+    }
+    train_df = get_data(name="train")
+    test_df = get_data(name="test")
+
+    fit_xgb_model(train_df, test_df, xgb_parms)
